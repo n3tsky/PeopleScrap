845b26ff2a1292d5839b43a506d9e4284689fc3e
n3tsky <n3tsky@protonmail.com>
2019-01-15 23:39:32 +0100

    updated settings.py

--- settings.py
+++ settings.py
@@ -2,17 +2,20 @@
 ROCKETREACH_API_KEY=""
 HUNTERIO_API_KEY=""
 
-
 #### Some constants
 # Web/requests
 USER_AGENT="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0"
 DEFAULT_WAIT=2
 
-# API
+### API
+# RocketReach
 ROCKETREACH_ACCOUNT_URL="https://api.rocketreach.co/v1/api/account"
 ROCKETREACH_SEARCH_URL="https://api.rocketreach.co/v1/api/search"
 ROCKETREACH_STATUS_URL="https://api.rocketreach.co/v1/api/checkStatus"
-ROCKETREACH_LOOKUP_URL="https://api.rocketreach.co/v1/api/search"
+ROCKETREACH_LOOKUP_URL="https://api.rocketreach.co/v1/api/lookupProfile"
+# HunterIO
+HUNTERIO_ACCOUNT_URL="https://api.hunter.io/v2/account"
+HUNTERIO_DOMAIN_URL="https://api.hunter.io/v2/domain-search"
 
 # Color
 GREEN="\033[0;32m"
